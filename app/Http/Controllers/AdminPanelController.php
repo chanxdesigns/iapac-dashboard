0e3e8d55e43dd7bd34d5e0f18340b1333ecd4953
Chanx Singha <chanx.designs@gmail.com>
2015-11-17 12:06:42 +0530

    Development Environment

---
+++ app/Http/Controllers/AdminPanelController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Dashboard\Http\Controllers;
+
+use Dashboard\RespCounter;
+use Illuminate\Http\Request;
+
+use Dashboard\Http\Requests;
+use Dashboard\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class AdminPanelController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function showAdminPanel () {
+        /*******************************
+         * Project ID
+         */
+        $projectid = DB::table('resp_counters')->lists('projectid');
+        $projectid = array_unique($projectid);
+        array_splice($projectid,0,0);
+
+        /*******************************
+         * Country
+         */
+        $country = DB::table('resp_counters')->lists('Languageid');
+        $country = array_unique($country);
+        array_splice($country,0,0);
+        return view('pages.adminpanel', compact('projectid','country'));
+    }
+
+    public function getData ($id,$country,$status) {
+        echo $id,$country,$status;
+    }
+}
