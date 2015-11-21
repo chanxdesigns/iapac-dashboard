24398cc5bee421af57e22eb3f399b552419c0d0e
Chanx Singha <chanx.designs@gmail.com>
2015-11-21 13:31:57 +0530

    Development Environment

---
+++ app/ProjectsList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dashboard;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectsList extends Model
+{
+    protected $table = "projects_list";
+    protected $fillable = [];
+}
