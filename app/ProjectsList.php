e0a85f41808bdd74c3404c39235ee89002e0378e
Chanx Singha <chanx.designs@gmail.com>
2016-03-04 18:02:17 +0530

    Added fillable columns

---
+++ app/ProjectsList.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dashboard;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectsList extends Model
+{
+    protected $table = "projects_list";
+    protected $fillable = [
+        'Project ID',
+        'Country',
+        'About',
+        'Vendor',
+        'C_Link',
+        'T_Link',
+        'Q_Link',
+        'Completes',
+        'Terminates',
+        'Quotafull'
+    ];
+}
