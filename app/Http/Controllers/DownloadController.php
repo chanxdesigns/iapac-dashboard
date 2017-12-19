c2e2793d7d1ca488ce7cde93790b47bd494e795c
chanxdesigns <chanx.designs@gmail.com>
2017-12-19 19:54:14 +0530

    Vendor column in CSV added

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -11,12 +11,12 @@ class DownloadController extends Controller
     public function createCsv ($status, $prjid) {
         $results = RespCounter::where('status', $status)->where('projectid', $prjid)->get();
         $csv = Writer::createFromFileObject(new \SplTempFileObject());
-        $csv->insertOne(['Project ID', 'Respondent ID', 'Status', 'Country', 'City', 'IP', 'Start Time', 'End Time', 'LOI (Seconds)']);
+        $csv->insertOne(['Project ID', 'Respondent ID', 'Vendor', 'Status', 'Country', 'City', 'IP', 'Start Time', 'End Time', 'LOI (Seconds)']);
         foreach ($results as $result) {
             $starttime = $result->starttime ? Carbon::createFromFormat('Y-m-d H:i:s',$result->starttime)->toDayDateTimeString() : null;
             $endtime = $result->enddate ? Carbon::createFromFormat('Y-m-d H:i:s',$result->enddate)->toDayDateTimeString() : null;
             $loi = $result->starttime ? Carbon::createFromFormat('Y-m-d H:i:s',$result->enddate)->diffInSeconds(Carbon::createFromFormat('Y-m-d H:i:s',$result->starttime)) : null;
-            $csv->insertOne([$result->projectid, $result->respid, $result->status, $result->Languageid, $result->city, $result->IP, $starttime, $endtime, $loi]);
+            $csv->insertOne([$result->projectid, $result->respid, $result->vendor, $result->status, $result->Languageid, $result->city, $result->IP, $starttime, $endtime, $loi]);
         }
         $dateTime = Carbon::now('Asia/Kolkata')->toDateString().'_'.preg_replace('/:/','',Carbon::now('Asia/Kolkata')->toTimeString());
         return $csv->output($status.'_'.$prjid.'_'.$dateTime.'.csv');
